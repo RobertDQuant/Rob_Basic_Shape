429ca3e6f5df5520c423452fd1eda33e4ea1e416
RobertDQuant <Robertdquant@gmail.com>
2017-02-22 16:07:36 -0500

    Add files via upload

---
+++ Rob_Basic Shape/sketch.js
@@ -0,0 +1,28 @@
+function setup() {
+  createCanvas(500,500);
+  background("#ff0066");
+}
+
+function draw() {
+ //The P
+ fill("white");
+ noStroke();
+ rect(150,280,20,110);
+ 
+ fill("white");
+ ellipse(180,310,60,60);
+ fill("#ff0066");
+ ellipse(180,310,40,40);
+ 
+ fill("white");
+ ellipse(280,350,70,70);
+ fill("#ff0066");
+ ellipse(270,350,60,60);
+ 
+ fill("white");
+ rect(250,271,15,50)
+ 
+ 
+ 
+ 
+}
